3cac9f4a590d4f6aef375ddbb934b59ab4c9ff93
Rohan <rohanmartin56711@gmail.com>
2025-11-21 10:04:48 +0530

    ci-cd spring boot project started

---
+++ src/main/java/com/rohan/ci_cd_study/CiCdStudyApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.ci_cd_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/ci_cd_study/CiCdStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.ci_cd_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
